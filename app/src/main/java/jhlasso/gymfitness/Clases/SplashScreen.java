486059c8d03fe16b137b36a5003cfe7a32b2bff6
Jhontan Javier Lasso Rios <jhlasso@umarina.edu.co>
2018-11-21 15:27:06 -0500

    Tercer Commit - SplashScreen

---
+++ app/src/main/java/jhlasso/gymfitness/Clases/SplashScreen.java
@@ -0,0 +1,33 @@
+package jhlasso.gymfitness.Clases;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import jhlasso.gymfitness.MainActivity;
+import jhlasso.gymfitness.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int DURACION_SPLASH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);    }
+
+}
